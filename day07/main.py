6efbe95231e10b463044370575ce00ace565b351
canozo <canwoahzo@gmail.com>
2018-07-26 15:33:52 -0600

    modify day07

--- day07/main.py
+++ day07/main.py
@@ -1,9 +1,27 @@
+import re
+
+
 def solve(puzzle):
     global_parents = []
     global_children = []
 
     for line in puzzle:
-        parent, children = line
+        parent, _, children = line
+        if children:
+            global_parents.append(parent)
+            global_children += children
+
+    for parent in global_parents:
+        if parent not in global_children:
+            return parent
+
+
+def solve_v2():
+    global_parents = []
+    global_children = []
+
+    for line in puzzle:
+        parent, weight, children = line
         if children:
             global_parents.append(parent)
             global_children += children
@@ -16,20 +34,27 @@ def solve(puzzle):
 def process_line(line):
     children = []
     temp_line = line.split(' -> ')
-    parent = temp_line[0].split(' (')[0]
+    parent, weight = temp_line[0].split(' (')
+    weight = int(re.sub('[ )\n]', '', weight))
 
     if len(temp_line) > 1:
         children = temp_line[1].split(', ')
         children[-1] = children[-1].strip()
-    return parent, children
+    return parent, weight, children
 
 
 def main():
     puzzle = []
+    part = int(input('part: '))
+
     with open('input.txt', 'r') as input_file:
         for line in input_file.readlines():
             puzzle.append(process_line(line))
-    print(solve(puzzle))
+
+    if part == 1:
+        print(solve(puzzle))
+    elif part == 2:
+        print(solve_v2(puzzle))
 
 
 if __name__ == '__main__':
