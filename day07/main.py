309a6cea1ca13e4c1860b00c99e087bda32e93fd
canozo <canwoahzo@gmail.com>
2018-07-28 19:22:49 -0600

    day 07 part 2

--- day07/main.py
+++ day07/main.py
@@ -16,19 +16,75 @@ def solve(puzzle):
             return parent
 
 
-def solve_v2():
-    global_parents = []
-    global_children = []
+def solve_v2(puzzle):
+    root = solve(puzzle)
+    culprit = find_culprit(root, puzzle)
+    _, weight, _ = find(culprit, puzzle)
+    return weight + find_weight_difference(culprit, puzzle)
 
-    for line in puzzle:
+
+def find_culprit(node, tree):
+    parent, weight, children = find(node, tree)
+    different = find_different(children, tree)
+
+    if not different:
+        return parent
+    else:
+        return find_culprit(different, tree)
+
+
+def get_weight(root, tree):
+    _, weight, children = find(root, tree)
+
+    if not children:
+        return weight
+    else:
+        for child in children:
+            weight += get_weight(child, tree)
+    return weight
+
+
+def get_children(root, tree):
+    _, _, children = find(root, tree)
+    return children
+
+
+def find_different(children, tree):
+    weights = []
+    for child in children:
+        weights.append(get_weight(child, tree))
+
+    # see if they are all the same
+    if weights.count(weights[0]) == len(weights):
+        return
+
+    # find the weight that is different
+    for i, weight in enumerate(weights):
+        if weights.count(weight) == 1:
+            return children[i]
+
+
+def find_weight_difference(brother, tree):
+    weights = []
+    for line in tree:
         parent, weight, children = line
-        if children:
-            global_parents.append(parent)
-            global_children += children
+        if brother in children:
+            for child in children:
+                weights.append(get_weight(child, tree))
+            normal, different = 0, 0
+            for weight in weights:
+                if weights.count(weight) > 1:
+                    normal = weight
+                else:
+                    different = weight
+            return normal - different
 
-    for parent in global_parents:
-        if parent not in global_children:
-            return parent
+
+def find(node, tree):
+    for line in tree:
+        parent, weight, children = line
+        if node == parent:
+            return parent, weight, children
 
 
 def process_line(line):
